b5f067148fabc123ca33ed82167dcb2570773ec5
Jerome Foster II <jfoster@wlapcs.org>
2017-03-31 04:07:15 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var x = 300;
+var y = 200;
+var img;
+
+function preload(){
+      img = loadImage("780efd13b3919fef6285f6c1d671fa09.jpg");
+}
+
+function setup() {
+    createCanvas(600, 400);
+    background(50);
+    
+}
+
+function draw() {
+  face(130, 80);
+  
+  if(mouseIsPressed){
+      face(mouseX, mouseY);
+  }
+}
+
+function face(x, y){
+    //outerface
+    imageMode(CENTER);
+    image(img, x, y, 100, 120);
+}
